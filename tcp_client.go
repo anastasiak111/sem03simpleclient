32acc76cfdb2f8851b0915cec0dfff805e2c145d
anastasiak111 <anastasiak@uia.no>
2023-04-21 10:28:08 +0000

    m

--- tcp_client.go
+++ tcp_client.go
@@ -12,13 +12,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
-		log.Fatal(err)
-	}
+                log.Fatal(err)
+        }
+
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
@@ -26,4 +29,7 @@ func main() {
 	} 
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
+
 }
+
+
